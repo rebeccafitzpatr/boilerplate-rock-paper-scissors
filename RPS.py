1c40557809e271d22d8135a9e974257daa959025
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2024-10-26 22:47:13 +0000

    random selection

--- RPS.py
+++ RPS.py
@@ -1,10 +1,15 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
+
+
+options = ["R", "P", "S"]
 
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
     guess = "R"
     if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+        #guess = opponent_history[-2]
+        guess = random.choice(options)
 
     return guess
