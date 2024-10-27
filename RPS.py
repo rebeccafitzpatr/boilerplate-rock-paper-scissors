ea79ed6011da38529c4b21b8cb6f7cbad74ec0b7
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2024-10-27 00:47:15 +0000

    acheive 100% win rate for quincy

--- RPS.py
+++ RPS.py
@@ -2,14 +2,37 @@
 import random
 
 
-options = ["R", "P", "S"]
+options = ['R', 'P', 'S']
+
+q_matrix = [[1,1,1],[1,1,1],[1,1,1]]
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play == "":
+        #reset q matrix
+        q_matrix[0] = [1,1,1]
+        q_matrix[1] = [1,1,1]
+        q_matrix[2] = [1,1,1]
+        opponent_history = []
 
+    if prev_play in options:
+        opponent_history.append(prev_play)
+
+    #notice the patterns betweenoppnents move.
+    if len(opponent_history) > 3:
+        #convert the moves to intergers
+        prev = options.index(opponent_history[-2])
+        curr = options.index(opponent_history[-1])
+        q_matrix[prev][curr] = q_matrix[prev][curr] * 1.25
+        q_matrix[curr][prev] = q_matrix[curr][prev] * 0.85
+
+
+    
     guess = "R"
-    if len(opponent_history) > 2:
+    if len(opponent_history) < 4:
         #guess = opponent_history[-2]
         guess = random.choice(options)
 
-    return guess
+    else:
+        #guess is the max value of the row corresponding to the last move of the opponent.
+        guess = options[q_matrix[curr].index(max(q_matrix[curr]))]
+    return guess 
